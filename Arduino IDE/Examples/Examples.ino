5fdcd0aead68f7c994573cc486535d71a0db0578
Omer Genc <omer.genc@litum.com>
2024-11-21 20:37:08 +0300

    ESP32  - Analog Input

--- Arduino IDE/Examples/Examples.ino
+++ Arduino IDE/Examples/Examples.ino
@@ -1,16 +1,26 @@
-#define LED 2
+#define LED         2
+#define BUTTON      0
+#define ANALOGPIN   0
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+int adcValue = 0;
+
+void setup()
+{
+    // put your setup code here, to run once:
+    delay(1000);
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    //pinMode(BUTTON, INPUT_PULLUP);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  delay(100);
-  digitalWrite(LED, LOW);
-  delay(100);
-  Serial.println("Omer Genc");
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    adcValue = analogRead(ANALOGPIN);
+    Serial.println(adcValue);
+
+    digitalWrite(LED, HIGH);
+    delay(250);
+    digitalWrite(LED, LOW);
+    delay(250);
 }
